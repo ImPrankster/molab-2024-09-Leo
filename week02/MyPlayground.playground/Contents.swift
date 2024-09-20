8944fc22b18d803542f6bca923e0ad4278e8c3dd
Leo Wang <meimprankster@gmail.com>
2024-09-20 09:52:06 -0400

    update week02 content

--- week02/MyPlayground.playground/Contents.swift
+++ week02/MyPlayground.playground/Contents.swift
@@ -4,6 +4,56 @@ let SIZE = 1024
 let SYMBOL_SIZE = 16
 let TEXT_POOL = "明月几时有把酒问青天不知天上宫阙今夕是何年我欲乘风归去又恐琼楼玉宇高处不胜寒起舞弄清影何似在人间转朱阁低绮户照无眠不应有恨何事长向别时圆人有悲欢离合月有阴晴圆缺此事古难全但愿人长久千里共婵娟"
 
+/**
+    苏轼
+ 
+    明月几时有？
+    把酒问青天。
+    不知天上宫阙，
+    今夕是何年。
+    我欲乘风归去，
+    又恐琼楼玉宇，
+    高处不胜寒。
+    起舞弄清影，
+    何似在人间。
+
+    转朱阁，
+    低绮户，
+    照无眠。
+    不应有恨，
+    何事长向别时圆？
+    人有悲欢离合，
+    月有阴晴圆缺，
+    此事古难全。
+    但愿人长久，
+    千里共婵娟。
+ 
+    Su Shi
+ 
+    Bright moon, when wast thou made?
+    Holding my cup, I ask of the blue sky.
+    I know not in heaven’s palaces
+    What year it is this night.
+    I long to ride the wind and return;
+    Yet fear that marble towers and jade houses,
+    So high, are over-cold.
+    I rise and dance and sport with limpid shades;
+    Better far to be among mankind.
+
+    Around the vermillion chamber,
+    Down in the silken windows,
+    She shines on the sleepless,
+    Surely with no ill-will.
+    Why then is the time of parting always at full moon?
+    Man has grief and joy, parting and reunion;
+    The moon has foul weather and fair, waxing and waning.
+    In this since ever there has been no perfection.
+    All I can wish is that we may have long life,
+    That a thousand miles apart we may share her beauty.
+
+    （Robert Kotewell & Norman Smith）
+*/
+
 let renderer = UIGraphicsImageRenderer(size: CGSize(width: SIZE, height: SIZE))
 
 var image = renderer.image { (context) in
