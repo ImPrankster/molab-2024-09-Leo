47c624fc3676381bc4c87f41f3856a83fc43e049
Leo Wang <meimprankster@gmail.com>
2024-10-20 16:51:02 -0400

    created week7 content

---
+++ week07/Time Machine/Time Machine/Time_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Time_MachineApp.swift
+//  Time Machine
+//
+//  Created by Ruofan Wang on 2024/10/18.
+//
+
+import SwiftUI
+
+@main
+struct Time_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
