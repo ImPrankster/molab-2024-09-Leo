508ad05fe378f59d38b2ca126dff04c6d6f1b6c9
Leo Wang <meimprankster@gmail.com>
2024-10-24 16:23:24 -0400

    finish v1 of time machine

--- week07/Time Machine/Time Machine/ContentView.swift
+++ week07/Time Machine/Time Machine/ContentView.swift
@@ -8,14 +8,76 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var forePickerIsPresented = false
+    @State var backPickerIsPresented = false
+    @State var foreResult: [UIImage] = []
+    @State var backResult: [UIImage] = []
+    @State var blendImage: [UIImage] = []
+
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            if foreResult.count < 1 {
+                Text("First, select a recent photo of yourself").font(
+                    .largeTitle
+                )
+                .fontWeight(.bold).fontDesign(.rounded)
+                Button("Select", systemImage: "photo.on.rectangle.angled") {
+                    forePickerIsPresented.toggle()
+                }.buttonStyle(.borderedProminent).controlSize(.large).font(
+                    .title2)
+            }
+            if foreResult.count >= 1 && backResult.count < 1 {
+                Text("Then, select a few landscape photo from the past").font(
+                    .largeTitle
+                )
+                .fontWeight(.bold).fontDesign(.rounded)
+                Button("Select", systemImage: "photo.on.rectangle.angled") {
+                    backPickerIsPresented.toggle()
+                }.buttonStyle(.borderedProminent).controlSize(.large).font(
+                    .title2)
+            }
+            if backResult.count >= 1 && foreResult.count >= 1
+                && blendImage.count < 1
+            {
+                Button("Generate Blend") {
+                    blendImage = backResult.map {
+                        img in
+                        BlendProcessor.shared.generateBlend(
+                            backImage: img, foreImage: foreResult[0])
+                        return BlendProcessor.shared.photoOutput
+                    }
+                }.buttonStyle(.borderedProminent).controlSize(.large).font(
+                    .title2)
+            }
+            if blendImage.count >= 1 {
+                VStack {
+                    TabView {
+                        ForEach(blendImage, id: \.self) {
+                            img in
+                            Image(uiImage: img)
+                                .resizable()
+                                .aspectRatio(contentMode: .fit)
+                        }
+                    }.tabViewStyle(.page)
+                    Button ("Reset") {
+                        blendImage.removeAll()
+                        foreResult.removeAll()
+                        backResult.removeAll()
+                    }
+                }
+            }
         }
         .padding()
+        .sheet(isPresented: $forePickerIsPresented) {
+            PhotoPicker(
+                pickerResult: $foreResult,
+                isPresented: $forePickerIsPresented)
+        }
+        .sheet(isPresented: $backPickerIsPresented) {
+            PhotoPicker(
+                pickerResult: $backResult,
+                isPresented: $backPickerIsPresented, limit: 20)
+        }
     }
 }
 
