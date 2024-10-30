ffff3705147bff61dd4a7c88c3420e1a3cfccb24
Leo Wang <meimprankster@gmail.com>
2024-10-29 20:35:09 -0400

    setup week 8 content

---
+++ week08/Animal Vision/Animal Vision/Animal_VisionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Animal_VisionApp.swift
+//  Animal Vision
+//
+//  Created by Ruofan Wang on 2024/10/29.
+//
+
+import SwiftUI
+
+@main
+struct Animal_VisionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
